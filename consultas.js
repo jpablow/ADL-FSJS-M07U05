01badbe1b87718d384f096401855bcaad8fb5da6
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-09-04 00:06:11 -0300

    Crea consulta

--- consultas.js
+++ consultas.js
@@ -9,3 +9,19 @@ const pool = new Pool({
   port: 5432,
   allowExitOnIdle: true,
 });
+
+const obtenerJoyas = async ({ limits, page, order_by }) => {
+  const [campo, direccion] = order_by.split('_');
+  const offset = (page - 1) * limits;
+  let consulta = format(
+    'SELECT * FROM inventario ORDER BY %s %s LIMIT %s OFFSET %s',
+    campo,
+    direccion,
+    limits,
+    offset
+  );
+  const { rows } = await pool.query(consulta);
+  return rows;
+};
+
+module.exports = { obtenerJoyas };
